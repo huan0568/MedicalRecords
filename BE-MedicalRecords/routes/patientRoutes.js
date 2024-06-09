4101160c4b5ca3b4076aeba239158f32ce095be0
huan.hoang <huan0568@gmail.com>
2024-06-09 21:05:21 +0700

    chore: change be

--- BE-MedicalRecords/routes/patientRoutes.js
+++ BE-MedicalRecords/routes/patientRoutes.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const patientController = require('../app/controllers/patientController');
 
 router.post('/create', patientController.createPatient);
-router.get('/', patientController.getAllPatients);
 router.get('/:id', patientController.getPatientById);
+router.get('/find/:id_patient', patientController.getPatientByIdPatient);
 router.put('/update/:id', patientController.updatePatientById);
 router.delete('/delete/:id', patientController.deletePatientById);
+router.get('/result/:is_result', patientController.findPatientByIsResult);
+router.get('/search/:key', patientController.searchPatient);
+router.get('/', patientController.getAllPatients);
 
 module.exports = router;
 
